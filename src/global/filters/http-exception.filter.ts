309cf3ebf31782d785353551b820d6d1eb408462
haeseung123 <showui96@gmail.com>
2023-11-29 20:53:58 +0900

    feat: response 및 예외 필터 적용
    - 요청 성공시 공통 형식 적용
    - 예외 필터 공통 형식 적용
    - http, jwt filter 생성
    - 응답 메시지 정의
    
    feat-#1

---
+++ src/global/filters/http-exception.filter.ts
@@ -0,0 +1,35 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+import { CommonErrorResponseDto } from '../dtos/common-error-response.dto';
+import { HttpExceptionResponseDto } from '../dtos/http-exception-response.dto';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+	catch(exception: HttpException, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const response = ctx.getResponse<Response>();
+		const request = ctx.getRequest<Request>();
+		const status = exception.getStatus();
+		const exceptionObj = exception.getResponse(); // ExceptionObj 타입의 객체
+
+		// const errorTest: CommonErrorResponseDto<HttpExceptionResponseDto> = {
+		// 	success: false,
+		// 	statusCode: status,
+		// 	error: exceptionObj['error'],
+		// 	message: exceptionObj['message'],
+		// 	timestamp: new Date().toISOString(), // 2023-10-27T14:30:20.123Z
+		// 	path: request.url,
+		// };
+
+		// response.status(status).json(errorTest);
+
+		response.status(status).json({
+			success: false,
+			statusCode: status,
+			error: exceptionObj['error'],
+			message: exceptionObj['message'],
+			timestamp: new Date().toISOString(), // 2023-10-27T14:30:20.123Z
+			path: request.url,
+		});
+	}
+}
